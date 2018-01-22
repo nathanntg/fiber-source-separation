9dc89dc2338e15426a2292fe88998b2097b1b53a
Nathan Perkins <nathan@maxmo.net>
2018-01-22 14:42:02 -0500

    Switch to log scale for figure 1

--- paper_model.m
+++ paper_model.m
@@ -16,15 +16,15 @@ fiber_profile_emi = sp_3d_to_2d(fiber_profile_emi); % symmetric, way faster
 %% figure 1: fiber distribution
 
 % constants
-number_of_fibers = 100;
+number_of_fibers = 250;
 volume = [1000; 1000; 400];
 position = [0.5; 0.5; 0.25];
-distribution = [125 0 0; 0 125 0; 0 0 5];
+distribution = [150 0 0; 0 150 0; 0 0 5];
 
 % generate fibers
 old_rng = rng; rng(1);
 [fibers, fiber_angles] = generate_fibers(number_of_fibers, ...
-    'fiber_distribution', distribution, 'volume', volume, ...
+    'fiber_distribution', distribution, 'angle_distribution', 0, 'volume', volume, ...
     'position', position);
 cells = generate_cells('volume', volume);
 rng(old_rng);
@@ -32,21 +32,30 @@ rng(old_rng);
 % calculate center
 center = volume .* position;
 
+% calculate cell mixing matrix
+fiber_mix = generate_realistic_rt_mixing(fibers, fiber_angles, cells, ...
+    fiber_profile_exc, fiber_profile_emi, 'figures', false, 'stats', false);
+cell_bright = max(fiber_mix, [], 1);
+[cell_bright_sort, idx] = sort(cell_bright ./ norm);
+
+% normalization
+%norm = max(fiber_profile_emi.volume(:)) * max(fiber_profile_exc.volume(:)); % based on a single fiber maximum potential round-trip brightness
+%norm = max(cell_bright); % based on all fibers, brightest cell
+norm = max(fiber_profile_emi.volume(:)) * max(fiber_profile_exc.volume(:));
+
 % open window
 figure;
 
 % plot cells
 threshold = 0.01;
-fiber_mix = generate_realistic_rt_mixing(fibers, fiber_angles, cells, ...
-    fiber_profile_exc, fiber_profile_emi, 'figures', false, 'stats', false);
-cell_bright = max(fiber_mix, [], 1);
-[cell_bright_sort, idx] = sort(cell_bright ./ max(cell_bright));
 idx = idx(cell_bright_sort >= threshold);
-cell_bright_sort = cell_bright_sort(cell_bright_sort > threshold);
-scatter(cells(1, idx) - center(1), cells(2, idx) - center(2), 128, cell_bright_sort, 'filled');
-colormap('jet');
-h = colorbar('Ticks', [0.02 0.5 1], 'TickLabels', {'0%', '50%', '100%'});
+cell_bright_sort = cell_bright_sort(cell_bright_sort >= threshold);
+scatter(cells(1, idx) - center(1), cells(2, idx) - center(2), 128, log10(cell_bright_sort), 'filled');
+caxis([-2 0]);
+colormap('parula');
+h = colorbar('Ticks', [-2 -1 0], 'TickLabels', {'10^{-2}', '10^{-1}', '10^0'});
 ylabel(h, 'Fluorescence yield [% of max]');
+title(sprintf('Fibers: %d; Cells: %d', number_of_fibers, length(cell_bright_sort)));
 
 % plot fibers
 hold on;
@@ -54,7 +63,7 @@ plot(fibers(1, :) - center(1), fibers(2, :) - center(2), '.', 'MarkerEdgeColor',
 hold off;
 axis square;
 l = max(max(abs(fibers(1, :) - center(1))), max(abs(fibers(2, :) - center(2))));
-l = 400; % ceil(l / 100) * 100;
+l = 500; % ceil(l / 100) * 100;
 xlim([-l l]); xticks([-l 0 l]); xlabel('x [{\mu}]');
 ylim([-l l]); yticks([-l 0 l]); ylabel('y [{\mu}]');
 
