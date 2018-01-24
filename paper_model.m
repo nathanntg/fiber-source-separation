5d716bb4b585d746dc62c49a9c490c9434401f43
Nathan Perkins <nathan@maxmo.net>
2018-01-23 21:58:34 -0500

    Plot relationship between fibers and cells (#1)
    Two potential figures:
    
    1. Showing number of cells visible to fibers, over different bundle sizes.
    2. Number of fibers that pick up a single neuron (again over different bundle sizes).

--- paper_model.m
+++ paper_model.m
@@ -54,12 +54,12 @@ scatter(cells(1, idx) - center(1), cells(2, idx) - center(2), 128, log10(cell_br
 caxis([-2 0]);
 colormap('parula');
 h = colorbar('Ticks', [-2 -1 0], 'TickLabels', {'10^{-2}', '10^{-1}', '10^0'});
-ylabel(h, 'Fluorescence yield [% of max]');
+ylabel(h, 'Fluorescence yield [frac. of max]');
 title(sprintf('Fibers: %d; Cells: %d', number_of_fibers, length(cell_bright_sort)));
 
 % plot fibers
 hold on;
-plot(fibers(1, :) - center(1), fibers(2, :) - center(2), '.', 'MarkerEdgeColor', [1, 0.0784, 0.576], 'MarkerSize', 12);
+plot(fibers(1, :) - center(1), fibers(2, :) - center(2), '.', 'MarkerEdgeColor', [1, 0.0784, 0.576], 'MarkerSize', 14);
 hold off;
 axis square;
 l = max(max(abs(fibers(1, :) - center(1))), max(abs(fibers(2, :) - center(2))));
@@ -69,25 +69,160 @@ ylim([-l l]); yticks([-l 0 l]); ylabel('y [{\mu}]');
 
 r = get(gcf, 'renderer'); print(gcf, '-depsc2', ['-' r], '~/Desktop/fig1-distribution.eps'); close;
 
+%% figure ?: number of cells seen
+
+% constants
+number_of_fibers = 50:50:1000;
+volume = [1200; 1200; 400];
+position = [0.5; 0.5; 0.25];
+distribution = [150 0 0; 0 150 0; 0 0 5];
+
+iters = 5;
+thresholds = [0.005 0.01 0.05 0.1 0.2];
+
+result_single = zeros(length(number_of_fibers), length(thresholds), iters);
+result_multi = zeros(length(number_of_fibers), length(thresholds), iters);
+
+% normalization
+norm = max(fiber_profile_emi.volume(:)) * max(fiber_profile_exc.volume(:));
+
+% generate fibers
+old_rng = rng; rng(0);
+cells = generate_cells('volume', volume);
+
+for i = 1:length(number_of_fibers)
+    for j = 1:iters
+        % generate fibers
+        [fibers, fiber_angles] = generate_fibers(number_of_fibers(i), ...
+            'fiber_distribution', distribution, 'volume', volume, ...
+            'position', position);
+        
+        fiber_mix = generate_realistic_rt_mixing(fibers, fiber_angles, cells, ...
+            fiber_profile_exc, fiber_profile_emi, 'figures', false, 'stats', false);
+        cell_bright = max(fiber_mix, [], 1);
+        
+        for k = 1:length(thresholds)
+            % swap threshold and iter index
+            result_single(i, k, j) = sum(cell_bright >= thresholds(k));
+            result_multi(i, k, j) = sum(sum(fiber_mix >= thresholds(k), 1) >= 2);
+        end
+    end
+end
+
+rng(old_rng);
+
+% plot it
+h = figure;
+h.Position(3) = h.Position(3) * 2;
+
+subplot(1, 2, 1);
+mn = mean(result_single, 3);
+% st = std(result_single, 0, 3);
+plot(number_of_fibers, mn);
+% hold on;
+% plot(number_of_fibers, mn + st, 'LineWidth', 1);
+% plot(number_of_fibers, mn - st, 'LineWidth', 1);
+% hold off;
+xlabel('Number of fibers');
+ylabel('Neurons above threshold'); ylim([0 ceil(mn(end, 3) / 100) * 100]);
+title('Visible to one or more fibers');
+
+l = cell(1, length(thresholds));
+for i = 1:length(thresholds)
+    l{i} = sprintf('\\geq %.1f%%', thresholds(i)*100);
+end
+legend('Location', 'NorthWest', l{:});
+
+subplot(1, 2, 2);
+mn = mean(result_multi, 3);
+plot(number_of_fibers, mn);
+xlabel('Number of fibers');
+ylabel('Neurons above threshold'); ylim([0 ceil(mn(end, 3) / 100) * 100]);
+title('Visible to two or more fibers');
+
+r = get(gcf, 'renderer'); print(gcf, '-depsc2', ['-' r], '~/Desktop/fig-cells.eps'); close;
+
+%% figure ?: number of fibers per cell
+
+% constants
+number_of_fibers = [50 250 500 750];
+volume = [1200; 1200; 400];
+position = [0.5; 0.5; 0.25];
+distribution = [150 0 0; 0 150 0; 0 0 5];
+
+top_neurons = 20; % top brightest neurons
+top_fibers = 10; % top most visible fibers
+iters = 5; % number of iterations
+norm = true; % normalize by brigtest fiber
+
+result = zeros(length(number_of_fibers), top_fibers, top_neurons, iters);
+
+% generate fibers
+old_rng = rng; rng(0);
+cells = generate_cells('volume', volume);
+
+for i = 1:length(number_of_fibers)
+    for j = 1:iters
+        % generate fibers
+        [fibers, fiber_angles] = generate_fibers(number_of_fibers(i), ...
+            'fiber_distribution', distribution, 'volume', volume, ...
+            'position', position);
+        
+        fiber_mix = generate_realistic_rt_mixing(fibers, fiber_angles, cells, ...
+            fiber_profile_exc, fiber_profile_emi, 'figures', false, 'stats', false);
+        
+        % figure out brightest neurons
+        cell_bright = max(fiber_mix, [], 1);
+        [cell_bright_sort, idx] = sort(cell_bright, 'descend');
+        
+        % visbility of top neurons
+        m = sort(fiber_mix(:, idx(1:top_neurons)), 'descend');
+        if norm
+            m = bsxfun(@rdivide, m, m(1, :));
+        end
+        
+        result(i, :, :, j) = reshape(m(1:top_fibers, :), 1, top_fibers, top_neurons, 1);
+    end
+end
+
+rng(old_rng);
+
+h = figure;
+h.Position = h.Position .* [1 1 2 2];
+
+for i = 1:length(number_of_fibers)
+    subplot(2, 2, i);
+    boxplot(reshape(squeeze(result(i, :, :, :)), top_fibers, [])' .* 100);
+    xlabel('Fibers in descending order of fluence');
+    if norm
+        ylabel('% of highest fluence fiber');
+    else
+        ylabel('% of max');
+    end
+    title(sprintf('%d fibers', number_of_fibers(i)));
+end
+
+r = get(gcf, 'renderer'); print(gcf, '-depsc2', ['-' r], '~/Desktop/fig-fibers.eps'); close;
+
 %% figure 2: sample signals
 
 close all;
 
 % constants
-number_of_fibers = 100;
+number_of_fibers = 250; % good results: 100
 volume = [1000; 1000; 400];
 position = [0.5; 0.5; 0.25];
-distribution = [25 0 0; 0 25 0; 0 0 5];
+distribution = [150 0 0; 0 150 0; 0 0 5]; % good results: [25 0 0; 0 25 0; 0 0 5]
 
-old_rng = rng; rng(0);
+old_rng = rng; rng(1);
 simulate_source_separation('mode', 'profile-rt', 'profile_exc', fiber_profile_exc, 'profile_fluor', fiber_profile_emi, ...
     'duration', 200, 'number_of_outputs', number_of_fibers, 'output_noise', @(n) normrnd(0, 0.05, 1, n), ... %
-    'params_fibers', {'fiber_distribution', distribution, 'volume', volume, 'position', position}, ...
+    'params_fibers', {'fiber_distribution', distribution, 'angle_distribution', 0, 'volume', volume, 'position', position}, ...
     'params_cells', {'volume', volume}, 'figures', 2);
 rng(old_rng);
 
-r = get(gcf, 'renderer'); print(gcf, '-depsc2', ['-' r], '~/Desktop/fig2-signals.eps'); close;
-close all;
+%r = get(gcf, 'renderer'); print(gcf, '-depsc2', ['-' r], '~/Desktop/fig2-signals.eps'); close;
+%close all;
 
 %% figure 3: auc
 
